a353a093c9d6443391efab866e2d17a7d9f0f86f
Saimul <saimul.hasnain@difx.io>
2023-04-24 16:05:20 +0400

    intial work

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const sqlInjectionReg = /(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)/i;
-
+/**
+ * This function will validate input for any SQL injection commands
+ * @param {*} joi 
+ * @returns 
+ */
 function sqlInjectionFilter (joi) {
 	return {
 		type: 'string',
