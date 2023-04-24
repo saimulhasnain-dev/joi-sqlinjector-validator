25ae18ea747a5bfe3db3e64086bb0c05298a3c22
Saimul <saimul.hasnain@difx.io>
2023-04-24 15:53:49 +0400

    intial work

---
+++ index.js
@@ -0,0 +1,22 @@
+const sqlInjectionReg = /(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)/i;
+
+function sqlInjectionFilter (joi) {
+	return {
+		type: 'string',
+		base: joi.string(),
+		rules: {
+			sqlInjectionFilter: {
+				validate (value, helpers) {
+					const containSQLInjection = sqlInjectionReg.test(value)
+					if (!containSQLInjection) {
+						return true;
+					}
+					return helpers.error("String shouldn't contain any SQL injection commands");
+				}
+			}
+		}
+	};
+}
+
+const JOI = require('joi');
+JOI.extend(sqlInjectionFilter);
