e075673dd91418c396739137699297a2220f60e4
Saimul <saimul.hasnain@difx.io>
2023-04-26 17:20:52 +0400

    bug fixes

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const sqlInjectionReg = /(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)/i;
 /**
- * This function will validate input for any SQL injection commands
+ * This function will return a Joi extension that validates input for any SQL injection commands.
  * @param {*} joi 
  * @returns 
  */
-function sqlInjectionFilter (joi) {
+function sqlInjectionFilter(joi) {
 	return {
-		type: 'string',
-		base: joi.string(),
-		rules: {
-			sqlInjectionFilter: {
-				validate (value, helpers) {
-					const containSQLInjection = sqlInjectionReg.test(value)
-					if (!containSQLInjection) {
-						return true;
-					}
-					return helpers.error("String shouldn't contain any SQL injection commands");
-				}
+	  type: 'string',
+	  base: joi.string(),
+	  rules: {
+		sqlInjectionFilter: {
+		  validate(value, helpers) {
+			const sqlInjectionReg = /(\s*([\0\b\'\"\n\r\t\%\_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)/i;
+			const containSQLInjection = sqlInjectionReg.test(value)
+			if (!containSQLInjection) {
+			  return value;
 			}
+			return helpers.error("String shouldn't contain any SQL injection commands");
+		  }
 		}
+	  }
 	};
-}
-
-const JOI = require('joi');
-export default JOI.extend(sqlInjectionFilter);
+  }
+  
+  const JOI = require('joi');
+  module.exports = JOI.extend(sqlInjectionFilter);
